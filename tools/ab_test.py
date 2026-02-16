a06e6ff2748c0cf039bc4655cf3c793aa6fef31e
Egor Lazarchuk <yegorlz@amazon.co.uk>
2026-02-16 14:51:30 +0000

    test

--- tools/ab_test.py
+++ tools/ab_test.py
@@ -120,12 +120,6 @@ IGNORED = [
     {"instance": "m6a.metal", "performance_test": "test_network_tcp_throughput"},
     # Network throughput on m7a.metal
     {"instance": "m7a.metal-48xl", "performance_test": "test_network_tcp_throughput"},
-    # vsock throughput on m7a.metal
-    {
-        "instance": "m7a.metal-48xl",
-        "performance_test": "test_vsock_throughput",
-        "mode": "g2h",
-    },
     # block latencies if guest uses async request submission
     {"fio_engine": "libaio", "metric": "clat_read"},
     {"fio_engine": "libaio", "metric": "clat_write"},
